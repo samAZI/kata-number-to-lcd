327876ad0840fb1df661647130e4cd7d8c99d410
Samy Azirou <samy@barclap.com>
2019-04-08 01:16:00 +0200

    refactor:export digit list file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-const lcdDigitList = [
-	require('./digits/0.js')(),
-	require('./digits/1.js')(),
-	require('./digits/2.js')(),
-	require('./digits/3.js')(),
-	require('./digits/4.js')(),
-	require('./digits/5.js')(),
-	require('./digits/6.js')(),
-	require('./digits/7.js')(),
-	require('./digits/8.js')(),
-	require('./digits/9.js')()
-]
+const lcdDigitList = require('./digits')
 
 const numberToLcd = function (number) {
 	return lcdDigitList[number]
