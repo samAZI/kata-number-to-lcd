5ea16e8fced71cfbc53a3fefde46d7d38101c45b
Samy Azirou <samy@misterfox.co>
2019-04-08 16:31:22 +0200

    refactor:Rename function and indentation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const lcdDigitList = require('./digits')
 
-const numberToLcd = function (number) {
-	return lcdDigitList[number]
+const numberToDigit = function (number) {
+    return lcdDigitList[number]
 }
 
-const manyNumberToLcd = function (numbers) {
-	let line1 = ''
-	let line2 = ''
-	let line3 = ''
+const manyNumberToDigit = function (numbers) {
+    let line1 = ''
+    let line2 = ''
+    let line3 = ''
 
-	for (let number of numbers) {
-		const digitSplitted = numberToLcd(Number(number)).split('\n')
-	  line1 += digitSplitted[1]
-	  line2 += digitSplitted[2]
-	  line3 += digitSplitted[3]
-	}
+    for (let number of numbers) {
+        const digitSplitted = numberToDigit(Number(number)).split('\n')
+        line1 += digitSplitted[1]
+        line2 += digitSplitted[2]
+        line3 += digitSplitted[3]
+    }
 
-	return `\n${line1}\n${line2}\n${line3}\n`
+    return `\n${line1}\n${line2}\n${line3}\n`
 }
 
 module.exports = {
-	numberToLcd,
-	manyNumberToLcd,
+    numberToDigit,
+    manyNumberToDigit,
 }
--- test/index.js
+++ test/index.js
@@ -1,42 +1,42 @@
-const assert = require('assert');
-const { manyNumberToLcd, numberToLcd } = require('../src/');
+const assert = require('assert')
+const { manyNumberToDigit, numberToDigit } = require('../src/')
 const digitList = [`
  _ 
 | |
 |_|
-`,`
+`, `
   
  |
  |
-`,`
+`, `
  _ 
  _|
 |_ 
-`,`
+`, `
  _ 
  _|
  _|
-`,`
+`, `
    
 |_|
   |
-`,`
+`, `
  _ 
 |_ 
  _|
-`,`
+`, `
  _ 
 |_ 
 |_|
-`,`
+`, `
  _ 
   |
   |
-`,`
+`, `
  _ 
 |_|
 |_|
-`,`
+`, `
  _ 
 |_|
  _|
@@ -45,58 +45,58 @@ const digitList = [`
 
 describe('Convert Number to LCD', function () {
 
-	describe('numberToLcd', function () {
+    describe('numberToDigit', function () {
 
-			function testDigitConvertion(expectedDigit, currentNumber) {
-				it(`number ${currentNumber}`, function (done) {
-					const result = numberToLcd(currentNumber)
+        function testDigitConvertion(expectedDigit, currentNumber) {
+            it(`number ${currentNumber}`, function (done) {
+                const result = numberToDigit(currentNumber)
 
-					assert.equal(result, expectedDigit)
-					done()
-				})
-			}
+                assert.equal(result, expectedDigit)
+                done()
+            })
+        }
 
-			digitList.forEach(function(digit, index) {
-				testDigitConvertion(digit, index)
-			})
+        digitList.forEach(function (digit, index) {
+            testDigitConvertion(digit, index)
+        })
 
-	})
+    })
 
-	describe('manyNumberToLcd', function () {
-		it ('should return 42 in digit', function () {
-			const result = manyNumberToLcd('42')
-			const expectedResult  = `
+    describe('manyNumberToDigit', function () {
+        it('should return 42 in digit', function () {
+            const result = manyNumberToDigit('42')
+            const expectedResult = `
     _ 
 |_| _|
   ||_ 
 `
-			assert.equal(result, expectedResult)
+            assert.equal(result, expectedResult)
 
-		})
+        })
 
-		it ('should return 4242 in digit', function () {
-			const result = manyNumberToLcd('4242')
-			const expectedResult  = `
+        it('should return 4242 in digit', function () {
+            const result = manyNumberToDigit('4242')
+            const expectedResult = `
     _     _ 
 |_| _||_| _|
   ||_   ||_ 
 `
-			assert.equal(result, expectedResult)
+            assert.equal(result, expectedResult)
 
-		})
+        })
 
-		it ('should return 123456789 in digit', function () {
-			const result = manyNumberToLcd('123456789')
-			const expectedResult  = `
+        it('should return 123456789 in digit', function () {
+            const result = manyNumberToDigit('123456789')
+            const expectedResult = `
    _  _     _  _  _  _  _ 
  | _| _||_||_ |_   ||_||_|
  ||_  _|  | _||_|  ||_| _|
 `
-			assert.equal(result, expectedResult)
+            assert.equal(result, expectedResult)
 
-		})
+        })
 
-	})
+    })
 
 
 })
