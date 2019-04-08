4bac9f0f2fe0e30553258b72c036bdc723e4c222
Samy Azirou <samy@barclap.com>
2019-04-09 00:07:57 +0200

    fix:Add error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const  {manyNumberToDigit } = require('./number-to-lcd')
 
+const USAGE = 'Usage ./src/index.js numbers[Integer] width[Integer](default:1) height[Integer](default:1)'
 const argv = process.argv.slice(2)
 
 if (argv.length === 0) {
-    console.info('Usage ./src/index.js numbers[Integer] width[Integer](default:1) height[Integer](default:1)')
+    console.info(USAGE)
 } else {
     const numbers = argv[0]
     const width = argv[1] || 1
     const height = argv[2] || 1
-    const lcdDigits = manyNumberToDigit(numbers,{ width, height })
-    console.info(lcdDigits)
+    try {
+        const lcdDigits = manyNumberToDigit(numbers,{ width, height })
+        console.info(lcdDigits)
+    } catch (err) {
+        console.error(err)
+        console.info(USAGE)
+    }
 }
